41fbd4e7975822226958adb2472dece99aae71d9
Mike Mamrila <mmamrila@gmail.com>
2022-02-22 21:07:42 -0700

    testing and commenting

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const button = document.getElementById('addTask');
 const container = document.getElementById('container');
 const priority = document.getElementById('priority');
-const priorityButton = document.getElementById('priorityButton');
+const priorityButton = document.getElementById('priorityId');
+
+
 
 button.addEventListener('click', function () {
   const newElement = document.createElement('div');
@@ -10,15 +12,18 @@ button.addEventListener('click', function () {
   container.append(newElement);
 });
 
+// Creates randomized 8 place hexidecimal ID for each task element created
 let priorityId = function () {
-  let s4 = function () {
-    return Math.floor((1 + Math.random()) * 0x10000)
+  let s8 = function () {
+    return Math.floor((Math.random()) * 0x100000000)
       .toString(16)
   }
-  return s4() + s4();
+  return s8();
 }
 
 console.log(priorityId());
+
+// Move task element to priority section on button click
 priorityButton.addEventListener('click', function () {
   priority.appendChild(container);
   console.log('clicked');
